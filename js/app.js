b5a5f35cb7e69ae521cb6db1b70cae5963980ab8
Nurgul Amat <nurgulamat212@gmail.com>
2021-12-12 10:24:12 -0600

    added menu bar  hide and show function for small screen device

---
+++ js/app.js
@@ -0,0 +1,15 @@
+let header = document.querySelector('.header');
+let menu = document.querySelector('#menu');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
+
