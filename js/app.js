0b9cca5badd5ae08c57498d1a0f64767eefbcdb2
Nurgul Amat <nurgulamat212@gmail.com>
2021-12-13 17:09:00 -0600

    added experience section

--- js/app.js
+++ js/app.js
@@ -7,9 +7,36 @@ menu.onclick = () =>{
 }
 
 
-
 window.onscroll = () =>{
     menu.classList.remove('fa-times');
     header.classList.remove('active');
 }
 
+
+
+var swiper = new Swiper(".experience-slider", {
+    loop:true, 
+    grabCursor:true,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        640: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+        1200: {
+            slidesPerView: 4,
+          },
+    },
+});
